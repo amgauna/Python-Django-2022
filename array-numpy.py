03dd2c00469e493763d886dd9962ab24b3c81c38
Ana Gauna <amgauna@gmail.com>
2022-02-07 16:36:58 -0300

    Update array-numpy.py

--- array-numpy.py
+++ array-numpy.py
@@ -19,3 +19,31 @@ np.arrange(0,30,3)
 np.linspace(0,10,4)
 np.random.randint(0,30,5)
                   
+# Operações com Numpy
+
+import numpy as np
+matriz = np.array([[1,2,3],[4,5,6]])
+matriz.max()
+matriz.argmax()
+matriz.mean()
+matriz.std()
+
+matriz3 = np.array([[2,4,6],[8,7,1]])
+matriz3.shape
+matriz + matriz3
+matriz * matriz3
+matriz3 ** 2
+
+# Seleção e Alteração de Valores
+matriz3[1][1]
+matriz3[0][2]
+matriz3[matriz3>4]
+matriz3[1][matriz3[1]>4]
+
+matriz3[1][0] = 16
+matriz3
+
+matriz3 = np.insert(matriz3,1,[0,0,1],axis=0)
+matriz3
+matriz3.shape
+
