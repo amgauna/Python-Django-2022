774a802aaa145f539b0ea6cfb22e05a4f3d1ef47
Ana Gauna <amgauna@gmail.com>
2022-02-07 18:19:21 -0300

    Create filtro-ajax.js

---
+++ filtro-ajax.js
@@ -0,0 +1,24 @@
+# Iniciaremos então o ajax com Javascript no sentido de realizar o filtro sem o refresh de página:
+
+(function(win,doc){
+    'use strict';
+
+    doc.querySelector('#country').addEventListener('change',async(event)=>{
+        event.preventDefault();
+        let req=await fetch('http://localhost:8000/countryFilter/',{
+            method:'POST',
+            headers:{
+                'Accept':'application/json',
+                'Content-Type':'application/json',
+                'X-CSRFToken':doc.querySelectorAll('input')[0].value
+            },
+            body:JSON.stringify({
+                'country':doc.querySelector('#country').value
+            })
+        });
+        let res=await req.json();
+        doc.querySelector('.result').innerHTML=res.data;
+    },false);
+
+})(window,document);
+
