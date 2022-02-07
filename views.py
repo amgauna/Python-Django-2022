fc1c9b2c695ea5367b63460b9be7ac180490bddc
Ana Gauna <amgauna@gmail.com>
2022-02-07 18:04:03 -0300

    Update views.py

--- views.py
+++ views.py
@@ -1,4 +1,5 @@
 # No arquivo views implementamos a lógica da aplicação, inclusive usando o Pandas e o Numpy.
+# Vamos testar o uso dos métodos do Pandas para manipular nossos dados:
 
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -6,9 +7,30 @@ import numpy as np
 import pandas as pd
 
 # Create your views here.
+def replaceCountries(x):
+    if(x=='Brazil'):
+        return x.replace('Brazil','Brasil')
+    else:
+        return x
 
 def home(request):
     df = pd.read_csv('app/static/data/netflix_titles.csv')
     data = {}
-    data['dados']=df.head().to_html()
+    #print(df.count())
+    #print(df.info())
+    """data['dados']=df[(df['release_year']>2009) & (df['country']=='Brazil')]\
+        .drop(['show_id','date_added'],axis=1)\
+        .dropna()\
+        .head(20)\
+        .to_html(index=False,classes=['table','table-striped','mt-5'])"""
+    #print(data['dados'].count())
+
+    """df['country'] = df['country'].apply(replaceCountries)
+    data['dados'] = df.to_html(index=False, classes=['table', 'table-striped', 'mt-5'])"""
+
+    grupo=df.groupby('country').count()
+    data['dados']=grupo[['title','release_year']]\
+        .sort_values(by='title',ascending=False)\
+        .to_html()
     return render(request,'index.html',data)
+
